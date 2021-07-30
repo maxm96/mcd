765cefaf9d75f6980a3aed992c6510de3fd8fa5e
Max McDaniel <maxm@piestar.com>
2021-07-29 19:18:32 -0500

    Init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+
+    <title>Laravel</title>
+</head>
+<body>
+<div class="top-bar">
+    <span class="title">mcd</span>
+    <span class="bar-link"><a href="{{ route('home') }}">Home</a></span>
+    <span class="bar-link"><a href="{{ route('chat') }}">Chat</a></span>
+    <span class="bar-link"><a href="{{ route('boards') }}">Boards</a></span>
+</div>
+</body>
+</html>
